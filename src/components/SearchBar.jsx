f0d36f803ef6eda95b8dbca04af73a1fb984bc06
Jacob Lopez <lopezjacob2003.jl@gmail.com>
2023-02-28 22:42:41 -0600

    Search Bar Fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,26 @@
-import React from 'react';
-import { useState } from 'react';
-import './SearchBar.css';
-import logo from '../assets/mobile-transparent--blue.png'
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+import { useState } from "react";
+import "./SearchBar.css";
+import logo from "../assets/mobile-transparent--blue.png";
+import SearchIcon from "@mui/icons-material/Search";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { IconButton } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const SearchBar = () => {
-    const [searchMode, setSearchMode] = useState(false);
-
-    const toggleSearch = () => {
-      setSearchMode(!searchMode);
-    };
-  
-    const searchIcon = searchMode ? (
-      <input type="text" placeholder="Search" />
-    ) : (
-      <SearchIcon onClick={toggleSearch} />
-    );
-
-    return (
-        <div className='SearchBar'>
-            <MenuIcon />
-            <img src={logo} alt='DreamSpeak Logo' className='Logo'/>
-            {searchIcon}
-        </div>
-    );
-}
+  return (
+    <div className="SearchBar">
+      <IconButton className="SearchBar-button">
+        <SettingsIcon />
+      </IconButton>
+      <Link to={"/home"}>
+        <img src={logo} alt="DreamSpeak Logo" className="Logo" />
+      </Link>
+      <IconButton className="SearchBar-button">
+        <SearchIcon />
+      </IconButton>
+    </div>
+  );
+};
 
 export default SearchBar;
